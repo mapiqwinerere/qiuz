64a556f2639feafabf1e8a8b5d26fa2896275d40
mapiqwinerere <toleg9410@gmail.com>
2023-09-27 17:27:50 +0800

    import

---
+++ interface.py
@@ -0,0 +1,5 @@
+#this file include code for interface of proggram for quiz with telegram bot
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QPropertyAnimation, QParallelAnimationGroup, QSequentialAnimationGroup, QThread, Qt, QTimer
+import io
